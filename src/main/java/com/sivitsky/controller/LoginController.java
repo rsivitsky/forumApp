0195781e6b5465cdbc2c4ea40c0fedb7382fe649
Ruslan <rsivitsky@gmail.com>
2018-02-10 02:53:56 +0300

    restore html and some changes in the another files

--- src/main/java/com/sivitsky/controller/LoginController.java
+++ src/main/java/com/sivitsky/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.sivitsky.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,17 @@ public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
         return new ModelAndView("login", "error", error);
+    }
+
+    @RequestMapping(value = "/restore", method = RequestMethod.GET)
+    public String getRestorePage(Model model) {
+        return "restore";
+    }
+
+    @RequestMapping(value = "/restore", method = RequestMethod.POST)
+    public String setRestorePage(String email) {
+        System.out.print(email);
+        return "redirect:/index";
     }
    @RequestMapping(value = "/index?logout", method = RequestMethod.GET)
     public ModelAndView getLogoutPage(@RequestParam Optional<String> logout) {
